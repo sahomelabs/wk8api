7dcb972c90db721616dc51429b34aa5f4a400ba2
sahomelabs <sahomelabs@gmail.com>
2023-12-17 16:35:15 -0500

    updat search update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react';
+import Search from './components/Search';
+import Forecast from './components/Forecast';
+import Current from './components/Current';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Welcome to WeatherApp</h1>
+      <p className="subtitle">Get real time weather information for your city.</p>
+      <Search />
+      <Current />
+      <Forecast />
     </div>
   );
 }
