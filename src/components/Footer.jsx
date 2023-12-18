2660959b97abe05f8f0dfcb753b6786b383767c4
sahomelabs <sahomelabs@gmail.com>
2023-12-17 19:07:01 -0500

    addedfooter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// Footer.jsx
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Weather App. All rights reserved. Design by Dan</p>
+    </footer>
+  );
+};
+
+export default Footer;
